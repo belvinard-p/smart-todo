e855bb95dde3b12b4ed1d7ea44cf1efc806851b7
belvip <pouadjeubelvi@gmail.com>
2025-12-29 06:23:42 +0000

    understand useState Hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,41 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Todo 1", description: "Desc 1", completed: false },
+    { id: 2, title: "Todo 2", description: "Desc 2", completed: true },
+    { id: 3, title: "Todo 3", description: "Desc 3", completed: false }
+  ])
 
   return (
     <div>
-      <h1>
-        Todo app
-      </h1>
+      <h1>Todo app</h1>
+
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <h3>{todo.title}</h3>
+            <p>{todo.description}</p>
+            <p>{todo.completed ? "Completed" : "Not Completed"}</p>
+          </div>
+        )
+      })}
+
+      <button onClick={() => {
+        setTodos((prevTodos) => [
+          ...prevTodos,
+          {
+            id: prevTodos.length + 1,
+            title: `Todo ${prevTodos.length + 1}`,
+            description: `Desc ${prevTodos.length + 1}`,
+            completed: false
+          }
+        ])
+      }}>
+        Add Todo
+      </button>
     </div>
-   
   )
 }
 
